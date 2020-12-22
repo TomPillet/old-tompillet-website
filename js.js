4794f5add26c03aeded47c3bb290bc71b6b4452e
TomPillet <76454484+TomPillet@users.noreply.github.com>
2020-12-22 05:24:28 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -9,9 +9,12 @@ var tabl_blender = ["bureau.png", "affiche_donuts.jpg", "Donuts.jpg", "voiture.p
 	tabl_pixelartgif = ["pumpkin-transform.gif", "pumpkin.gif", "bato.gif"],
 	tabl_articles_pdf = ["article-exo-v1.pdf", "article-exo-v2.pdf", "article-satellite-v1.pdf", "article-satellite-v2.pdf"],
 	tabl_articles_jpg = ["exo-v1.jpg", "exo-v2.jpg", "satellite-v1.jpg", "satellite-v2.jpg"],
-	tabl_cartes = ["blood moon.png", "invasion de sauterelles.png", "manif.png", "peste.png", "pleine lune.png", "tempête.png"];
+	tabl_cartes = ["blood moon.png", "invasion de sauterelles.png", "manif.png", "peste.png", "pleine lune.png", "tempête.png"],
+	tabl_securingdata_clév1 = ["diapo1.jpg", "diapo2.jpg", "diapo3.jpg", "diapo4.jpg", "diapo5.jpg"],
+	tabl_securingdata_clév2 = ["diapo1.jpg", "diapo2.jpg", "diapo3.jpg", "diapo4.jpg", "diapo5.jpg"],
+	tabl_securingdata_shield = ["diapo1.jpg", "diapo2.jpg", "diapo3.jpg", "diapo4.jpg", "diapo5.jpg", "diapo6.jpg"];
 
-var i=0, j=0, g=0, h=0, k=0;
+var i=0, j=0, g=0, h=0, k=0, secu1=0, secu2=0, secu3=0;
 
 
 // RESEAUX SOCIAUX
@@ -183,5 +186,79 @@ function nav_cartes(sens) {
 			img.src = "medias/jeu_de_societe/cartes_events/" + tabl_cartes[k];
 		}
 		return k;
+	}
+}
+
+
+// DIAPORAMA SECURING DATA
+function nav_securingdata_clév1(sens) {
+	var img = document.getElementById("clév1");
+	if (sens == "left") {
+		if (secu1 == 0) {
+			secu1 = tabl_securingdata_clév1.length-1;
+			img.src = "medias/securingdata/clév1/" + tabl_securingdata_clév1[secu1];
+		} else {
+			k--;
+			img.src = "medias/securingdata/clév1/" + tabl_securingdata_clév1[secu1];
+		}
+		return secu1;
+
+	} else if (sens == "right") {
+		if (secu1 == 4) {
+			secu1 = 0;
+			img.src = "medias/securingdata/clév1/" + tabl_securingdata_clév1[secu1];
+		} else {
+		 	secu1++;
+			img.src = "medias/securingdata/clév1/" + tabl_securingdata_clév1[secu1];
+		}
+		return secu1;
+	}
+}
+
+function nav_securingdata_clév2(sens) {
+	var img = document.getElementById("clév2");
+	if (sens == "left") {
+		if (secu2 == 0) {
+			secu2 = tabl_securingdata_clév2.length-1;
+			img.src = "medias/securingdata/clév2/" + tabl_securingdata_clév2[secu2];
+		} else {
+			secu2--;
+			img.src = "medias/securingdata/clév2/" + tabl_securingdata_clév2[secu2];
+		}
+		return secu2;
+
+	} else if (sens == "right") {
+		if (secu2 == 4) {
+			secu2 = 0;
+			img.src = "medias/securingdata/clév2/" + tabl_securingdata_clév2[secu2];
+		} else {
+			secu2++;
+			img.src = "medias/securingdata/clév2/" + tabl_securingdata_clév2[secu2];
+		}
+		return secu2;
+	}
+}
+
+function nav_securingdata_shield(sens) {
+	var img = document.getElementById("shield");
+	if (sens == "left") {
+		if (secu3 == 0) {
+			secu3 = tabl_securingdata_shield.length-1;
+			img.src = "medias/securingdata/shield/" + tabl_securingdata_shield[secu3];
+		} else {
+			secu3--;
+			img.src = "medias/securingdata/shield/" + tabl_securingdata_shield[secu3];
+		}
+		return secu3;
+
+	} else if (sens == "right") {
+		if (secu3 == 5) {
+			secu3 = 0;
+			img.src = "medias/securingdata/shield/" + tabl_securingdata_shield[secu3];
+		} else {
+			secu3++;
+			img.src = "medias/securingdata/shield/" + tabl_securingdata_shield[secu3];
+		}
+		return secu3;
 	}
 }
